3bd0d964a0469d98f4984ae30705e940cf78d87b
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-01-25 14:08:50 +0530

    develop cart modal

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { Fragment } from "react"
 
 import Header from "./Layout/Header"
 import Meals from "./Meals/Meals"
+import Cart from "./Cart/Cart"
 
 const App = () => {
     return(
         <Fragment>
+            <Cart />
             <Header />
             <main>
                 <Meals />
