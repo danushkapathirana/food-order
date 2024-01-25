c2a40da73f4dec8585fe179194fd62c9bab1a9be
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-01-25 12:53:51 +0530

    develop summary section

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react"
+
+import MealsSummary from "./MealsSummary/MealsSummary"
+
+const Meals = () => {
+    return(
+        <Fragment>
+            <MealsSummary />
+        </Fragment>
+    )
+}
+
+export default Meals
